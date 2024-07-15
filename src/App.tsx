cb9d63468d678cf20ca69de934bca22e31fd89aa
avocadianmage <claytongotwals@gmail.com>
2024-07-14 21:59:43 -0500

    ensure in dev contexts we're still only polling once

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,20 @@ const darkTheme = createTheme({
 const PollIntervalMs = 30000;
 const getPromisedTimeout = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
+let IsPollingStarted = false;
+
 const App = () => {
     const [followedStreams, setFollowedStreams] = useState<Stream[]>([]);
 
     const pollFollowedStreams = async (isFirstPoll: boolean) => {
+        console.log('[' + new Date().toLocaleString() + '] Polling Twitch for followed streams.');
+
         const streamInfos = await Twitch.GetFollowedStreams();
         setFollowedStreams((prev) => {
             return streamInfos.map((si, index) => ({
                 ...si,
                 selected:
-                    (index === 0 && isFirstPoll) || // On first poll select first stream by default 
+                    (index === 0 && isFirstPoll) || // On first poll select first stream by default
                     (prev.find((prevSI) => prevSI.user_id === si.user_id)?.selected ?? false),
             }));
         });
@@ -33,6 +37,9 @@ const App = () => {
     };
 
     useEffect(() => {
+        if (IsPollingStarted) return;
+        IsPollingStarted = true;
+
         Twitch.Authenticate();
         pollFollowedStreams(true);
     }, []);
