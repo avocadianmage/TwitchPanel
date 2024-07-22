24693faa8a8eb42335ae27999cbf11fed2076369
avocadianmage <claytongotwals@gmail.com>
2024-07-22 01:40:11 -0500

    titlebar styling overhaul

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,39 +1,43 @@
-import { Coffee, GitHub, LiveTv } from "@mui/icons-material";
-import { Box, IconButton, Tooltip, Typography } from "@mui/material";
+import { Coffee, GitHub, LiveTv } from '@mui/icons-material';
+import { AppBar, Box, IconButton, Toolbar, Tooltip, Typography } from '@mui/material';
 import { repository, funding } from '../../package.json';
 
 export const TitleBar = () => {
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                justifyContent: 'space-between',
-            }}
-            padding='8px 8px 8px 16px'
-        >
-            <Typography
-                fontFamily='Noto Sans'
-                fontSize='1.5rem'
-                fontWeight='600'
-                color='primary.light'
-            >
-                <LiveTv sx={{ color: 'secondary.dark' }} />
-                <Box component='span' sx={{ verticalAlign: 'text-bottom' }}>
-                    &nbsp;TWITCH PANEL
-                </Box>
-            </Typography>
-            <Box component='span'>
-                <Tooltip title='GitHub' disableInteractive>
-                    <IconButton href={repository.url} target='_blank'>
-                        <GitHub />
-                    </IconButton>
-                </Tooltip>
-                <Tooltip title='Buy me a coffee' disableInteractive>
-                    <IconButton href={funding} target='_blank'>
-                        <Coffee />
-                    </IconButton>
-                </Tooltip>
-            </Box>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position='static'>
+                <Toolbar disableGutters sx={{ p: '0 16px' }}>
+                    <LiveTv sx={{ color: 'secondary.main' }} />
+                    <Typography
+                        variant='h6'
+                        noWrap
+                        sx={{
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            color: 'inherit',
+                            textDecoration: 'none',
+                            marginLeft: '1px',
+                            marginTop: '1px',
+                        }}
+                    >
+                        TWITCH
+                        <Box component='span' sx={{ color: 'secondary.main' }}>
+                            PANEL
+                        </Box>
+                    </Typography>
+                    <Tooltip title='GitHub' disableInteractive>
+                        <IconButton href={repository.url} target='_blank' color='inherit'>
+                            <GitHub />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title='Buy me a coffee' disableInteractive>
+                        <IconButton href={funding} target='_blank' color='inherit'>
+                            <Coffee />
+                        </IconButton>
+                    </Tooltip>
+                </Toolbar>
+            </AppBar>
         </Box>
     );
 };
